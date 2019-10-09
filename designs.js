cb4bbb849641ae3fe7dba5e68ab900a6010c14df
omar-bakr <omar.bakr@student.guc.edu.eg>
2019-10-09 17:38:53 +0200

    The whole task

--- designs.js
+++ designs.js
@@ -1,9 +1,38 @@
 // Select color input
 // Select size input
-
+var row,column,color;
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-  // Your code goes here!
+$('#submit_button').click(function(event){
+  event.preventDefault();
+  row=$('#inputHeight').val();
+  column=$('#inputWidth').val();
+  makeGrid(row,column);
+})
+function makeGrid(row,column) {
+
+  $('tr').remove();
+  
+  for(var i=0;i<row;i++){
+    $('#pixelCanvas').append('<tr id=table'+i+'></tr>');
+    for(var j=0;j<column;j++)
+    {
+      $('#table'+i).append('<td></td>');
+    }
+
+  }
+  $('td').click(function coloring(){
+    color=$('#colorPicker').val();
+
+    if($(this).attr('style')){
+      $(this).removeAttr('style')
+    }
+    else{
+      $(this).attr('style','background-color:'+color);
+    }
+  })
+ 
 }
+
+    
 
